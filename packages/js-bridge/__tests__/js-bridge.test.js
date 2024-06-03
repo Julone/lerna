d1cade9ade63037c201cefefd1f624bef6828929
lizulong <julone@qq.com>
2024-06-03 14:28:34 +0800

    commit

--- packages/js-bridge/__tests__/js-bridge.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const jsBridge = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(jsBridge(), 'Hello from jsBridge');
-console.info('jsBridge tests passed');
