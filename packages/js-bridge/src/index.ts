4707ff01a8e1670262ce3ed61bbaba0c78786423
lizulong <julone@qq.com>
2024-06-03 12:10:16 +0800

    test

--- packages/js-bridge/src/index.ts
+++ packages/js-bridge/src/index.ts
@@ -1,3 +1,4 @@
 export {default as iggqaJsBridgeParent} from './parent';
 export {default as iggqaJsBridgeChild} from './child';
 export * from './utils'
+console.log('%cpackages\js-bridge\src\index.ts:4 1', 'color: #007acc;', 1);
