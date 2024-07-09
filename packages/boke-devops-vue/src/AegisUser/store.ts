680141b8eb1c2fdb3f3fe9ed4d59069c5aeb21ee
lizulong <julone@qq.com>
2024-07-09 10:44:50 +0800

    fix: 修复props merge写法错误

--- packages/boke-devops-vue/src/AegisUser/store.ts
+++ packages/boke-devops-vue/src/AegisUser/store.ts
@@ -1,5 +1,6 @@
 import { computed, toRef,  getCurrentInstance } from "vue"
 import { globalConfig, globalState } from "./../store"
+import { merge } from "lodash-es"
 
 export const NAME_KEY = computed(() => globalConfig.avatar.user_name)
 export const AVATAR_KEY = computed(() => globalConfig.avatar.user_avatar)
@@ -8,10 +9,12 @@ export const USERID_KEY = computed(() => globalConfig.avatar.user_id)
 
 export const useCustomProps = () => {
     const props = getCurrentInstance()?.attrs as any;
-    let NAME_KEY = toRef(props?.customProps || {}, 'user_name', globalConfig.avatar.user_name)
-    let AVATAR_KEY = toRef(props?.customProps || {}, 'user_avatar', globalConfig.avatar.user_avatar)
-    let DEPT_KEY = toRef(props?.customProps || {}, 'dept_name', globalConfig.avatar.dept_name)
-    let USERID_KEY = toRef(props?.customProps || {}, 'user_id', globalConfig.avatar.user_id)
+    const mergedProps = merge(globalConfig.avatar, props?.customProps || {});
+
+    let NAME_KEY = toRef(mergedProps, 'user_name')
+    let AVATAR_KEY = toRef(mergedProps, 'user_avatar')
+    let DEPT_KEY = toRef(mergedProps, 'dept_name')
+    let USERID_KEY = toRef(mergedProps, 'user_id')
     const global_always_avatar_is_hihglight = globalState.global_always_avatar_is_hihglight
     return {
         NAME_KEY,
