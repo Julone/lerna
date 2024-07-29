c8814d4179c7a92e2a780f854e95e5ba7f3dc5fb
lizulong <julone@qq.com>
2024-07-29 18:29:05 +0800

    chore: remove package.json import

--- packages/boke-devops-vue/src/Provider/Provider.vue
+++ packages/boke-devops-vue/src/Provider/Provider.vue
@@ -4,7 +4,6 @@
 <script setup lang="tsx">
 import {provide} from "vue"
 import {mergeConfig} from "./store"
-import {version} from "./../../package.json"
 
 const props = defineProps({
   "globalConfig": {
@@ -22,7 +21,7 @@ provide("globalConfig", gc)
 provide("globalState", gs)
 
 console.log(
-      ` %c ${'@devops/boke-devops-vue'}  %c 由 BOKE运维部 构建 v${version} `,
+      ` %c ${'@devops/boke-devops-vue'}  %c 由 BOKE运维部 构建 `,
       'color: #fff; background: #ea3c37; padding:5px 0;',
       'background: #333; color: #fff; padding:5px 0;',
     );
